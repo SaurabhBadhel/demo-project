4eee512af68b4fa7b2e987e6218dc34581d68859
Saurabh Badhel <52860063+SaurabhBadhel@users.noreply.github.com>
2024-07-29 13:45:33 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_ENDPOINTS = {
+    SUBMIT: 'https://eefg5vinf0.execute-api.ap-south-1.amazonaws.com/test/submit',
+    HISTORY: 'https://eefg5vinf0.execute-api.ap-south-1.amazonaws.com/test/history'
+};
